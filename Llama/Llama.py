2893411ec8b683cd5dbf6ee957e866f875a5aa47
maryamdkh <m.dadkhah99@gmail.com>
2025-04-07 19:35:05 +0330

    .

--- Llama/Llama.py
+++ Llama/Llama.py
@@ -151,7 +151,7 @@ def generate_analysis(model, tokenizer, transcription: str, shap_values: Union[D
     Returns:
         str: The generated analysis text
     """
-    token_shap_pairs = get_pairs(shap_values.data[0], shap_values.shap_values, shap_index)
+    token_shap_pairs = get_pairs(shap_values.data[0], shap_values.values, shap_index)
 
     prompt = system_prompt1.format(text=transcription, shap_values=json.dumps(token_shap_pairs, indent=2))
     inputs1 = tokenizer(prompt, return_tensors="pt").to(model.device)
